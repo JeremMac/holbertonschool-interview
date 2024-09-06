68742e17919305d8ed4f58184fe4aee3496cf0b8
JeremMac <macal.jerem@gmail.com>
2024-09-06 11:48:36 +0200

    Changed code.

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,43 +1,71 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "sort.h"
 
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void sift_down(int *array, size_t size, size_t index)
-{
-    size_t largest = index;
-    size_t left = 2 * index + 1;
-    size_t right = 2 * index + 2;
-
-    if (left < size && array[left] > array[largest])
-        largest = left;
-
-    if (right < size && array[right] > array[largest])
-        largest = right;
-
-    if (largest != index)
-    {
-        swap(&array[index], &array[largest]);
-        print_array(array, size);
-        sift_down(array, size, largest);
-    }
-}
-
+/**
+ * heap_sort - sorts an array of ints in asc order using Heap sort algorithm
+ * @array: array
+ * @size: array size
+ */
 void heap_sort(int *array, size_t size)
 {
-    for (int i = size / 2 - 1; i >= 0; i--)
-        sift_down(array, size, i);
+	size_t i;
 
-    for (size_t i = size - 1; i > 0; i--)
-    {
-        swap(&array[0], &array[i]);
-        print_array(array, size);
-        sift_down(array, i, 0);
-    }
+	if (!array)
+	{
+		return;
+	}
+
+	for (i = size / 2; i > 0; i--) /* build max heap */
+	{
+		heapify(array, size, i - 1, size); /* heapify */
+	}
+
+	for (i = size - 1; i > 0; i--) /* swap root with last node */
+	{
+		swap(&array[0], &array[i], array, size); /* swap */
+		heapify(array, i, 0, size); /* heapify */
+	}
+}
+/**
+ * heapify - organizes the array into max heap
+ * @arr: array
+ * @n: size
+ * @i: index
+ * @size: original size
+ */
+void heapify(int *arr, int n, int i, size_t size)
+{
+	int higher = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < n && arr[left] > arr[higher]) /* left child is higher */
+	{
+		higher = left;
+	}
+
+	if (right < n && arr[right] > arr[higher]) /* right child is higher */
+	{
+		higher = right;
+	}
+
+	if (higher != i) /* if higher is not the root */
+	{
+		swap(&arr[i], &arr[higher], arr, size); /* swap */
+		heapify(arr, n, higher, size);
+		/* recursively heapify the affected sub-tree */
+	}
+}
+/**
+ * swap - swap pointer position
+ * @a: pointer to swap
+ * @b: pointer to swap
+ * @array: array
+ * @n: size of array
+ */
+void swap(int *a, int *b, int *array, size_t n)
+{
+	int aux = *a;
+	*a = *b;
+	*b = aux;
+	print_array(array, n);
 }
--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -1,9 +1,11 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H_
+#define _SORT_H_
 
-//#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
-
-#endif /* SORT_H */
+void heapify(int *arr, int n, int i, size_t size);
+void swap(int *a, int *b, int *array, size_t n);
+#endif /* _SORT_H_ */
