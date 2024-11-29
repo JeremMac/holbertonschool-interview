3354e7359a1480ddcedede2fecbf46fb04aceeed
JeremMac <macal.jerem@gmail.com>
2024-11-29 15:30:02 +0100

    Added all wildcmp files for correction.

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - Compares two strings and checks if they are identical.
+ * @s1: The first string.
+ * @s2: The second string, which can contain the '*' wildcard.
+ *
+ * Return: 1 if the strings are considered identical, otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+    if (*s2 == '*')
+    {
+        // Move to the next character in s2
+        return wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2));
+    }
+    if (*s1 == '\0' && *s2 == '\0')
+    {
+        return 1;
+    }
+    if (*s1 == *s2)
+    {
+        return wildcmp(s1 + 1, s2 + 1);
+    }
+    return 0;
+}
