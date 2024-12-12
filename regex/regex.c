786f897b359b2e460361075e332d2bd5a9376f58
JeremMac <macal.jerem@gmail.com>
2024-12-12 23:52:43 +0100

    Fix: betty coding style.

--- regex/regex.c
+++ regex/regex.c
@@ -9,27 +9,27 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-    if (*pattern == '\0')
-    {
-        return (*str == '\0');
-    }
+	if (*pattern == '\0')
+	{
+		return (*str == '\0');
+	}
 
-    if (*(pattern + 1) == '*')
-    {
-        if (regex_match(str, pattern + 2))
-        {
-            return (1);
-        }
-        if ((*str != '\0' && (*str == *pattern || *pattern == '.')) &&
-            regex_match(str + 1, pattern))
-        {
-            return (1);
-        }
-    }
-    else if (*str != '\0' && (*str == *pattern || *pattern == '.'))
-    {
-        return (regex_match(str + 1, pattern + 1));
-    }
+	if (*(pattern + 1) == '*')
+	{
+		if (regex_match(str, pattern + 2))
+		{
+			return (1);
+		}
+		if ((*str != '\0' && (*str == *pattern || *pattern == '.')) &&
+				regex_match(str + 1, pattern))
+		{
+			return (1);
+		}
+	}
+	else if (*str != '\0' && (*str == *pattern || *pattern == '.'))
+	{
+		return (regex_match(str + 1, pattern + 1));
+	}
 
-    return (0);
+	return (0);
 }
